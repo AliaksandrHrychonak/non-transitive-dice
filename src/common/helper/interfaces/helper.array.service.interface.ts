a02ba41750adfc4ba0d685ed38f8196d41b166bf
AliaksandrHrychonak <@icloud.com>
2025-03-16 22:39:09 +0300

    feat(helper): add generateNumberArray and generateOptionArray to array service

--- src/common/helper/interfaces/helper.array.service.interface.ts
+++ src/common/helper/interfaces/helper.array.service.interface.ts
@@ -1,3 +1,7 @@
 export interface IHelperArrayService {
     parseCommaSeparatedNumbers(input: string): number[];
+
+    generateNumberArray(input: number): Array<{ value: string; title: string }>;
+
+    generateOptionArray(arr: number[]): Array<{ value: string; title: string }>;
 }
