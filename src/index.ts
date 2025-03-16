dde4279f99770cedac6f07f1bf317ed4928368a1
AliaksandrHrychonak <@icloud.com>
2025-03-16 22:40:59 +0300

    build(core): update service initialization and dependencies

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ import {
     HelperEncryptionService,
     HelperNumberService,
     HelperProbabilityService,
-    HelperStringService,
 } from './common/helper/services';
 import { MessageService } from './common/message/services';
 import { ManagerService } from './common/manager/services';
@@ -13,31 +12,36 @@ import { GameService } from './core/game/services';
 import { PlayerService } from './core/player/services';
 import { GameController } from './core/game/controller';
 import { DiceTableService } from './core/dice-table/services';
+import { DiceValidatorService } from './core/dice/services';
 
 if (require.main === module) {
     const configService = ConfigService.getInstance(Config);
-
-    const helperStringService = new HelperStringService();
+    
     const helperArrayService = new HelperArrayService();
     const helperEncryptionService = new HelperEncryptionService();
     const helperProbabilityService = new HelperProbabilityService();
     const helperNumberService = new HelperNumberService();
 
-    const messageService = new MessageService(helperStringService, configService);
-    const managerService = new ManagerService(configService, helperNumberService, messageService);
-    const diceTableService = new DiceTableService(helperProbabilityService);
+    const messageService = new MessageService(configService);
+    const managerService = new ManagerService(configService, helperNumberService, helperArrayService, messageService);
+    const diceValidatorService = new DiceValidatorService(
+        configService,
+        helperArrayService,
+        managerService,
+        messageService
+    );
+    const diceTableService = new DiceTableService(configService, helperProbabilityService);
     const playerService = new PlayerService(helperEncryptionService);
     const gameService = new GameService(
+        configService,
+        helperEncryptionService,
         messageService,
         managerService,
-        helperArrayService,
-        helperEncryptionService,
         playerService,
-        diceTableService,
-        configService
+        diceTableService
     );
 
-    const gameController = new GameController(gameService, configService);
+    const gameController = new GameController(gameService, diceValidatorService);
 
     const args = process.argv.slice(2);
 
