f90373a95615ae28d416ccf87c8c421b07bd84ba
AliaksandrHrychonak <@icloud.com>
2025-03-16 22:40:47 +0300

    refactor(dice-table): add config service and improve formatting

--- src/core/dice-table/services/dice-table.service.ts
+++ src/core/dice-table/services/dice-table.service.ts
@@ -1,13 +1,25 @@
 import { IDiceTableService } from '../interfaces';
 import { IHelperProbabilityService } from '@common/helper/interfaces';
 import CliTable from 'cli-table3';
+import { IConfigService } from '@common/config/interfaces';
+import { Config } from '../../../config/index';
 
 export class DiceTableService implements IDiceTableService {
-    constructor(private readonly helperProbabilityService: IHelperProbabilityService) {}
+    private config = Config;
+    private configTable = Config['table'];
+
+    constructor(
+        private readonly configService: IConfigService,
+        private readonly helperProbabilityService: IHelperProbabilityService
+    ) {
+        this.config = this.configService.getConfig();
+        this.configTable = this.config.table;
+    }
 
     public createProbabilityTable(dices: number[][]): string {
         const probabilities = this.helperProbabilityService.calculateMatrix(dices);
-        const headers = ['User dice', ...dices.map((d) => d.toString())];
+
+        const headers = ['Probability Table', ...dices.map((d) => d.toString())];
 
         const table = new CliTable({
             head: headers,
@@ -16,7 +28,11 @@ export class DiceTableService implements IDiceTableService {
         dices.forEach((dice, i) => {
             table.push([
                 dice.toString(),
-                ...probabilities[i].map((prob, j) => (i === j ? `- (${prob.toFixed(4)})` : prob.toFixed(4))),
+                ...probabilities[i].map((prob, j) =>
+                    i === j
+                        ? `- (${prob.toFixed(this.configTable.decimalPlaces)})`
+                        : prob.toFixed(this.configTable.decimalPlaces)
+                ),
             ]);
         });
 
