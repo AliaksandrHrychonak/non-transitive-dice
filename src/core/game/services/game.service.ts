119f246ca72400ecd50bccf666c09c5e9d23cccb
AliaksandrHrychonak <@icloud.com>
2025-03-16 22:40:13 +0300

    refactor(game): move dice validation logic to separate service

--- src/core/game/services/game.service.ts
+++ src/core/game/services/game.service.ts
@@ -1,33 +1,26 @@
 import { IGameService } from '@core/game/interfeces';
 import { IMessageService } from '@common/message/interfaces';
 import { IManagerService } from '@common/manager/interfaces';
-import { IHelperArrayService, IHelperEncryptionService } from '@common/helper/interfaces';
+import { IHelperEncryptionService } from '@common/helper/interfaces';
 import type { Config } from '@config/index';
 import { IConfigService } from '@common/config/interfaces';
 import { IPlayerService } from '@core/player/interfeces';
 import { IDiceTableService } from '@core/dice-table/interfaces';
 
 export class GameService implements IGameService {
-    private readonly configDiceLength: { min: number; max: number };
-    private readonly configDiceValues: { min: number; max: number };
-    private readonly configDiceFaces: { min: number; max: number };
     private readonly configGame: typeof this.config.game;
     private readonly firstMoveOptions: { value: string; title: string }[];
     private readonly config: Config;
 
     constructor(
+        private readonly configService: IConfigService,
+        private readonly helperEncryptionService: IHelperEncryptionService,
         private readonly messageService: IMessageService,
         private readonly managerService: IManagerService,
-        private readonly helperArrayService: IHelperArrayService,
-        private readonly helperEncryptionService: IHelperEncryptionService,
         private readonly playerService: IPlayerService,
-        private readonly diceTableService: IDiceTableService,
-        private readonly configService: IConfigService
+        private readonly diceTableService: IDiceTableService
     ) {
         this.config = this.configService.getConfig();
-        this.configDiceLength = this.config.game.dices;
-        this.configDiceValues = this.config.dice.values;
-        this.configDiceFaces = this.config.dice.faces;
         this.configGame = this.config.game;
         this.firstMoveOptions = this.managerService.createManagerOptionArray(this.configGame.firstMoveChoice);
     }
@@ -259,10 +252,6 @@ export class GameService implements IGameService {
         return this.managerService.exit();
     }
 
-    public parseDice(dice: string[]): number[][] {
-        return dice.map((e) => this.helperArrayService.parseCommaSeparatedNumbers(e));
-    }
-
     public showThrowResultHint(result: number, isPersonFirst: boolean): void {
         return this.managerService.showHint(
             this.messageService.getMessage('game.timeThrow', {
@@ -270,109 +259,5 @@ export class GameService implements IGameService {
                 isPersonFirst,
             })
         );
-    }
-
-    private validateDiceLength(dice: number[][]): void {
-        if (!dice) {
-            this.managerService.showError(
-                this.messageService.getMessage('errors.tooFewDice', {
-                    required: this.configDiceLength.min,
-                    current: 0,
-                    needed: this.configDiceLength.min,
-                    dice: `${dice}`,
-                })
-            );
-        }
-
-        if (dice.length < this.configDiceLength.min) {
-            this.managerService.showError(
-                this.messageService.getMessage('errors.tooFewDice', {
-                    required: this.configDiceLength.min,
-                    current: dice.length,
-                    needed: this.configDiceLength.min - dice.length,
-                    dice: `${dice}`,
-                })
-            );
-        }
-
-        if (dice.length > this.configDiceLength.max) {
-            this.managerService.showError(
-                this.messageService.getMessage('errors.tooManyDice', {
-                    maximum: this.configDiceLength.max,
-                    current: dice.length,
-                    excess: dice.length - this.configDiceLength.max,
-                    dice: `${dice}`,
-                })
-            );
-        }
-    }
-
-    private validateDiceValue(faces: number[], value: number): void {
-        if (!Number.isInteger(value)) {
-            this.managerService.showError(
-                this.messageService.getMessage('errors.invalidValueTypeDice', {
-                    value: value,
-                    dice: `${faces}`,
-                })
-            );
-        }
-
-        if (value < this.configDiceValues.min || value > this.configDiceValues.max) {
-            this.managerService.showError(
-                this.messageService.getMessage('errors.invalidValueDice', {
-                    min: this.configDiceValues.min,
-                    max: this.configDiceValues.max,
-                    value: value,
-                    dice: `${faces}`,
-                })
-            );
-        }
-    }
-
-    private validateDiceFaces(arr: number[][]): void {
-        arr.forEach((faces) => {
-            if (!faces) {
-                this.managerService.showError(
-                    this.messageService.getMessage('errors.tooFewFacesDice', {
-                        required: this.configDiceFaces.min,
-                        current: 0,
-                        needed: this.configDiceFaces.min,
-                        dice: `${faces}`,
-                    })
-                );
-            }
-
-            if (faces.length < this.configDiceFaces.min) {
-                this.managerService.showError(
-                    this.messageService.getMessage('errors.tooFewFacesDice', {
-                        required: this.configDiceFaces.min,
-                        current: faces.length,
-                        needed: this.configDiceFaces.min - faces.length,
-                        dice: `${faces}`,
-                    })
-                );
-            }
-
-            if (faces.length > this.configDiceFaces.max) {
-                this.managerService.showError(
-                    this.messageService.getMessage('errors.tooManyFacesDice', {
-                        maximum: this.configDiceFaces.max,
-                        current: faces.length,
-                        excess: faces.length - this.configDiceFaces.max,
-                        dice: `${faces}`,
-                    })
-                );
-            }
-
-            faces.forEach((value) => this.validateDiceValue(faces, value));
-        });
-    }
-
-    public createDice(dice: string[]): number[][] {
-        const parsedDice = this.parseDice(dice);
-        this.validateDiceLength(parsedDice);
-        this.validateDiceFaces(parsedDice);
-
-        return parsedDice;
     }
 }
